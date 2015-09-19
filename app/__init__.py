bf6ec965889c63dd465dc918ea887e4f5e582ce1
Shane Moore <shanewmoore@gmail.com>
2015-09-19 15:38:22 -0400

    Crop working (I think)

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,38 @@
-from flask import Flask 
-from flask import Response, request, jsonify
+import subprocess
+import uuid
+import os
+from typing import Dict, Any
+from flask import Flask, Response, request, send_file
 app = Flask(__name__)
 
 
-@app.route('/add_subtitles', methods=['POST'])
-def index() -> Response:
-    with request.files['video'] as video:
-        subtitles = request.values['subtitles']
-        for sub in subtitles:
-            video = add_subtitles(video, sub)
-    return jsonify(video=video)
+@app.route('/crop', methods=['POST'])
+def crop() -> Response:
+    print(request.files)
+    print(request.values)
+    video = request.files['video']
+    print(video)
+    filename = os.path.join('tmp', str(uuid.uuid4().hex) + '.webm')
+    video.save(filename)
+    crop = request.values
+    crop_video(crop, filename)
+    return send_file(os.path.join('..', filename), mimetype='video/webm')
 
 
-def add_subtitles(video, sub):
+def subtitle() -> Response:
+    video = request.files['video']
+    filename = os.path.join('tmp', uuid.uuid4() + '.webm')
+    video.save(filename)
+    subtitles = request.values['subtitles']
+    for sub in subtitles:
+        video = add_subtitles(sub, filename)
+    return send_file(os.path.join('..', filename), mimetype='video/webm')
+
+
+def crop_video(crop: Dict[str, int], filename: str):
+    crop_params = 'crop={}:{}:{}:{}'.format(crop['width'], crop['height'], crop['horizontal'], crop['vertical'])
+    subprocess.call(['ffmpeg', '-i', filename, '-vf', crop_params, filename])
+
+
+def add_subtitles(sub: Dict[str, Any], filename: str):
     return video
