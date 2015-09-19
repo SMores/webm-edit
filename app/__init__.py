76256d1f77d6fa200cb9d7577ec86870b3e6ed3b
Shane Moore <shanewmoore@gmail.com>
2015-09-19 12:59:39 -0400

    Simple parsing for request object for adding subtitles

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask 
+from flask import Response, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def index() -> str:
-    return "Hello World"
+@app.route('/add_subtitles', methods=['POST'])
+def index() -> Response:
+    with request.files['video'] as video:
+        subtitles = request.values['subtitles']
+        for sub in subtitles:
+            video = add_subtitles(video, sub)
+    return jsonify(video=video)
+
+
+def add_subtitles(video, sub):
+    return video
