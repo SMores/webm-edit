b29dee115ad76c7b1f88883bd4da80ef68edca19
dashbrown <dtb63@cornell.edu>
2015-09-19 14:33:00 -0400

    create srt and merge it with webm

---
+++ subtitle/subtitle.py
@@ -0,0 +1,51 @@
+from subprocess import call
+from os.path import basename
+
+json = {
+  # "video": @file.webm,
+  "subtitles": [ {
+    "text": "Ayyyyy lmao",
+    "start_time": 4,
+    "end_time": 12
+  }]
+}
+
+def create_srt(json):
+    with open(json["video"].name + ".srt", 'w') as f:
+        # print f
+        for sub,i in zip(json["subtitles"],xrange(1,len(json["subtitles"])+1)):
+            f.write(str(i) + "\n")
+            minutes_start = sub["start_time"] / 60
+            seconds_start = sub["start_time"] % 60
+            minutes_end = sub["end_time"] / 60
+            seconds_end = sub["end_time"] % 60
+            f.write("00:" + pad_int_to_str(minutes_start) + ":" + pad_int_to_str(seconds_start) + ",000" + \
+            " --> 00:" + pad_int_to_str(minutes_end) + ":" + pad_int_to_str(seconds_end) + ",000\n")
+            f.write(sub["text"] + "\n\n")
+        return json["video"].name + ".srt"
+
+def pad_int_to_str(i):
+    if i >= 10:
+        return str(i)
+    return "0" + str(i)
+
+def merge_webm_srt(json):
+    srt = create_srt(json)
+    command = "ffmpeg -i " + json["video"].name + " -codec:v libvpx -vf " + \
+              "subtitles=" + srt + " out.webm"
+    call(command)
+    return command
+
+if __name__ == '__main__':
+    j = {
+        "video":open("morty.webm"),
+        "subtitles" : [
+            {"start_time": 0, "end_time" : 3, "text": "ayyy"},
+            {"start_time": 4, "end_time" : 7, "text": "ayyy"},
+            {"start_time": 9, "end_time" : 13, "text": "ayyy"},
+            {"start_time": 15, "end_time" : 16, "text": "ayyy"},
+            {"start_time": 20, "end_time" : 30, "text": "ayyy"},
+            {"start_time": 105, "end_time" : 120, "text": "ayyy"}
+        ]}
+    print merge_webm_srt(j)
+
