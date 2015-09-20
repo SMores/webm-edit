f96b52c85759e3ef2a4ad032779ed7fe12a0ab96
Shane Moore <shanewmoore@gmail.com>
2015-09-20 04:41:37 -0400

    Subtitles work

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import subprocess
 import uuid
 import os
+import json
 from typing import Dict, Any, List
 from flask import Flask, Response, request, send_file
 app = Flask(__name__)
@@ -19,9 +20,9 @@ def crop() -> Response:
 @app.route('/subtitle', methods=['POST'])
 def subtitle() -> Response:
     video = request.files['video']
-    filename = os.path.join('tmp', uuid.uuid4() + '.webm')
+    filename = os.path.join('tmp', str(uuid.uuid4().hex) + '.webm')
     video.save(filename)
-    subtitles = request.values['subtitles']
+    subtitles = json.loads(request.values['subtitles'])
     add_subtitles(subtitles, filename)
     return send_file(os.path.join('..', filename.split('.')[0] + '-subbed.webm'), mimetype='video/webm')
 
@@ -36,24 +37,26 @@ def crop_video(crop: Dict[str, int], filename: str):
 
 
 def create_srt(subtitles: List[Dict[str, Any]], filename: str) -> str:
-    for sub, i in zip("subtitles", range(1, len("subtitles") + 1)):
-        f.write(str(i) + "\n")
-        milli_start = sub["start_time"] % 1000
-        sub["start_time"] /= 1000
-        minutes_start = sub["start_time"] / 60
-        seconds_start = sub["start_time"] % 60
-        milli_end = sub["end_time"] % 1000
-        sub["end_time"] /= 1000            
-        minutes_end = sub["end_time"] / 60
-        seconds_end = sub["end_time"] % 60
-        f.write("00:{0:02d}:{0:02d},{0:03d} --> 00:{0:02d}:{0:02d},{0:03d}\n".format(
-                minutes_start, seconds_start, milli_start, minutes_end, seconds_end, milli_end))
-        f.write(sub["text"] + "\n\n")
-    return filename + ".srt"
+    srt_filename = filename.split('.')[0] + '.srt'
+    with open(srt_filename, 'w') as f:
+        for i, sub in enumerate(subtitles):
+            f.write(str(i) + "\n")
+            milli_start = sub["start_time"] % 1000
+            sub["start_time"] //= 1000
+            minutes_start = sub["start_time"] // 60
+            seconds_start = sub["start_time"] % 60
+            milli_end = sub["end_time"] % 1000
+            sub["end_time"] //= 1000
+            minutes_end = sub["end_time"] // 60
+            seconds_end = sub["end_time"] % 60
+            f.write("00:{0:02d}:{1:02d},{2:03d} --> 00:{3:02d}:{4:02d},{5:03d}\n".format(
+                    minutes_start, seconds_start, milli_start, minutes_end, seconds_end, milli_end))
+            f.write(sub["text"] + "\n\n")
+    return srt_filename
 
 
 def add_subtitles(subtitles: List[Dict[str, Any]], filename: str):
     srt_filename = create_srt(subtitles, filename)
-    command = "ffmpeg -nostdin -y -i {0} -codec:v libvpx -qmin 0 -qmax 50 -crf 5 -b:v 1M -c:a libvorbis -vf " + \
-              "subtitles={1} {2}.webm".format(filename, srt_filename, filename.split('.')[0] + '-subbed.webm')
+    command = ("ffmpeg -nostdin -y -i {0} -codec:v libvpx -qmin 0 -qmax 50 -crf 5 -b:v 1M -c:a libvorbis -vf " +
+               "subtitles={1} {2}").format(filename, srt_filename, filename.split('.')[0] + '-subbed.webm')
     subprocess.call(command.split())
