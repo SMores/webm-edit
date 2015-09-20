0e00f98b581699ccabd13da383e3dd7e880ea393
Shane Moore <shanewmoore@gmail.com>
2015-09-20 02:49:23 -0400

    Try adding subtitles to server

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import subprocess
 import uuid
 import os
-from typing import Dict, Any
+from typing import Dict, Any, List
 from flask import Flask, Response, request, send_file
 app = Flask(__name__)
 
@@ -16,14 +16,14 @@ def crop() -> Response:
     return send_file(os.path.join('..', filename.split('.')[0] + '-cropped.webm'), mimetype='video/webm')
 
 
+@app.route('/subtitle', methods=['POST'])
 def subtitle() -> Response:
     video = request.files['video']
     filename = os.path.join('tmp', uuid.uuid4() + '.webm')
     video.save(filename)
     subtitles = request.values['subtitles']
-    for sub in subtitles:
-        video = add_subtitles(sub, filename)
-    return send_file(os.path.join('..', filename), mimetype='video/webm')
+    add_subtitles(subtitles, filename)
+    return send_file(os.path.join('..', filename.split('.')[0] + '-subbed.webm'), mimetype='video/webm')
 
 
 def crop_video(crop: Dict[str, int], filename: str):
@@ -35,5 +35,25 @@ def crop_video(crop: Dict[str, int], filename: str):
                      crop_params, out_filename])
 
 
-def add_subtitles(sub: Dict[str, Any], filename: str):
-    return video
+def create_srt(subtitles: List[Dict[str, Any]], filename: str) -> str:
+    for sub, i in zip("subtitles", range(1, len("subtitles") + 1)):
+        f.write(str(i) + "\n")
+        milli_start = sub["start_time"] % 1000
+        sub["start_time"] /= 1000
+        minutes_start = sub["start_time"] / 60
+        seconds_start = sub["start_time"] % 60
+        milli_end = sub["end_time"] % 1000
+        sub["end_time"] /= 1000            
+        minutes_end = sub["end_time"] / 60
+        seconds_end = sub["end_time"] % 60
+        f.write("00:{0:02d}:{0:02d},{0:03d} --> 00:{0:02d}:{0:02d},{0:03d}\n".format(
+                minutes_start, seconds_start, milli_start, minutes_end, seconds_end, milli_end))
+        f.write(sub["text"] + "\n\n")
+    return filename + ".srt"
+
+
+def add_subtitles(subtitles: List[Dict[str, Any]], filename: str):
+    srt_filename = create_srt(subtitles, filename)
+    command = "ffmpeg -nostdin -y -i {0} -codec:v libvpx -qmin 0 -qmax 50 -crf 5 -b:v 1M -c:a libvorbis -vf " + \
+              "subtitles={1} {2}.webm".format(filename, srt_filename, filename.split('.')[0] + '-subbed.webm')
+    subprocess.call(command.split())
